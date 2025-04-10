2c2357afe5d6a4577fe86cd73d0d3b094f22862d
Keyner de Avila <kddeavila3@gmail.com>
2025-04-10 00:17:34 -0500

    first commit with Astro

---
+++ src/constants/cursors.js
@@ -0,0 +1,14 @@
+export const cursors = [
+    { title: "Default", classname: "cursor-default" },
+    { title: "Pointer", classname: "cursor-pointer" },
+    { title: "Wait", classname: "cursor-wait" },
+    { title: "Text", classname: "cursor-text" },
+    { title: "Move", classname: "cursor-move" },
+    { title: "Crosshair", classname: "cursor-crosshair" },
+    { title: "Not Allowed", classname: "cursor-not-allowed" },
+    { title: "Help", classname: "cursor-help" },
+    { title: "Progress", classname: "cursor-progress" },
+    { title: "Cell", classname: "cursor-cell" },
+    { title: "Zoom In", classname: "cursor-zoom-in" },
+    { title: "Zoom Out", classname: "cursor-zoom-out" },
+];
